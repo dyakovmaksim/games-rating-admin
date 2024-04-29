c01d9b33430c8459e12381be17c19d064d0580b0
dyakovmaksim <pochta3478@mail.ru>
2024-04-29 18:33:04 +0500

    add contrllers and html

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const mainRoute = require('./routes/main');
 const gamesRouter = require('./routes/games');
 
@@ -6,6 +7,7 @@ const PORT = 3000;
 const app = express();
 
 app.use(mainRoute, gamesRouter);
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}`)
