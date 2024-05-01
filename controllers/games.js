d29fdb10f054ec7495c56b7856359aac649b013f
dyakovmaksim <pochta3478@mail.ru>
2024-05-01 16:21:03 +0500

    add middlewares

--- controllers/games.js
+++ controllers/games.js
@@ -1,31 +1,46 @@
-const { readData, writeData } = require("../utils/data");
+const { writeData } = require("../utils/data");
 
-
-const getAllGames = async (req, res) => {
-    const games = await readData("./data/games.json");
-    if (!games) {
+const addGameController = async (req, res) => {
+    // Проверяем, есть ли уже в списке игра с таким же названием
+    req.isNew = !Boolean(req.games.find(item => item.title === req.body.title));
+    // Если игра, которую хотим добавить, новая (её не было в списке)
+    if (req.isNew) {
+        // Добавляем объект с данными о новой игре
+        const inArray = req.games.map(item => Number(item.id));
+        let maximalId;
+        if (inArray.length > 0) {
+          maximalId = Math.max(...inArray);
+        } else {
+          maximalId = 0;
+        }
+        req.updatedObject = {
+          id: maximalId + 1,
+          title: req.body.title,
+          image: req.body.image,
+          link: req.body.link,
+          description: req.body.description
+        };
+        // Добавляем данные о новой игре в список с другими играми
+        req.games = [...req.games, req.updatedObject];
+    } else {
         res.status(400);
-        res.send({
-            status: "error",
-            message: "Нет игр в базе данных. Добавьте игру."
-        });
-        return;
+        res.send({ status: "error", message: "Игра с таким именем уже есть." });
+        return
     }
-    req.games = games;
+    // Записываем обновлённый список игр в файл
+    await writeData("./data/games.json", req.games);
+    // В качестве ответа отправляем объект с двумя полями
+    res.send({
+        games: req.games, // Обновлённый список со всеми играми
+        updated: req.updatedObject // Новая добавленная игра
+    });
+}
+
+const sendAllGames = async (req, res) => {
     res.send(req.games);
 };
 
 const deleteGame = async (req, res) => {
-    const games = await readData("./data/games.json");
-    if (!games) {
-        res.status(400);
-        res.send({
-            status: "error",
-            message: "Нет игр в базе данных. Добавьте игру.",
-        });
-        return;
-    }
-    req.games = games;
 
     const id = Number(req.params.id);
 
@@ -44,6 +59,7 @@ const deleteGame = async (req, res) => {
 }
 
 module.exports = {
-    getAllGames,
-    deleteGame
+    sendAllGames,
+    deleteGame,
+    addGameController
 }
